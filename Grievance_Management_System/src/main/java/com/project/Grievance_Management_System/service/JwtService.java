0884f64b0429e38430adf8e273a81fbde2837fd9
Smera Tharakan <tharakansmera@gmail.com>
2024-10-24 15:59:42 +0530

    set expiration to an hour

--- Grievance_Management_System/src/main/java/com/project/Grievance_Management_System/service/JwtService.java
+++ Grievance_Management_System/src/main/java/com/project/Grievance_Management_System/service/JwtService.java
@@ -45,7 +45,7 @@ public class JwtService {
             .add(claims)
             .subject(email)
             .issuedAt(new Date(System.currentTimeMillis()))
-            .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 100))
+            .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
             .and()
             .signWith(getKey())
             .compact();
