acfa2f3f11a9cb2d9e6fdd1f0d8acc86990b9bab
Smera Tharakan <tharakansmera@gmail.com>
2024-10-24 11:44:13 +0530

    changed register request from userController to authController

--- Grievance_Management_System/src/main/java/com/project/Grievance_Management_System/controller/UsersController.java
+++ Grievance_Management_System/src/main/java/com/project/Grievance_Management_System/controller/UsersController.java
@@ -5,7 +5,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,17 +56,6 @@ public class UsersController {
     @GetMapping ("/email/{email}")
     public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email){
         return usersService.getUserByEmail(email);
-    }
-
-    @PostMapping ("/create")
-    public ResponseEntity<String> createUsers(@RequestBody UserDto userDto){
-        return usersService.createUser(userDto);
-    }
-
-    @PreAuthorize("hasAnyRole('ADMIN', 'SUPERVISOR')")
-    @PostMapping ("/createEmployee")
-    public ResponseEntity<String> createUserEmployee(@RequestBody UserDto userDto){
-        return usersService.createEmployee(userDto);
     }
 
     @PutMapping ("/updateUsername/{id}")
