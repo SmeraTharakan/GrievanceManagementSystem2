e744586198d9593f74f40271cb5e7a8c33d1d73a
Smera Tharakan <tharakansmera@gmail.com>
2024-10-25 15:53:23 +0530

    added about component

---
+++ frontend/src/About/About.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='about'>
+      <div className='passage'>
+        <h2>What is Grievance Management System?</h2>
+        <p>Grievance management is the process of handling, 
+          managing, responding to, and reporting customer 
+          grievances. This process requires a streamlined 
+          approach and constant monitoring to ensure faster 
+          resolutions.</p>
+      </div>
+      <div className='passage'>
+        <h2>Assign the tickets to the right agent and notify 
+          them promptly</h2>
+        <p>When a customer submits a complaint, it’s promptly 
+          routed to the appropriate department or assigned to the 
+          relevant team member. This ensures that each issue reaches 
+          the right personnel equipped to address it effectively. The 
+          assigned department then takes ownership, working diligently 
+          to resolve the complaint as quickly and thoroughly as 
+          possible.</p>
+      </div>
+      <div className='passage'>
+        <h2>Provide a Platform that facilitates workplace collaboration</h2>
+        <p>With a large team of agents, chaos and confusion can be 
+          commonplace without a proper customer complaint management 
+          system in place. Grievance management System, like GMS
+          , comes with a set of features that are specifically 
+          designed to make workplace collaboration easier.</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default About;
